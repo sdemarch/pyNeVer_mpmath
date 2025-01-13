33c41ebc7c4bfa80f7959a173f882582caeec246
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-01-13 11:08:19 +0100

    Update mpmath tensor functions

--- pynever/__init__.py
+++ pynever/__init__.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -7,9 +8,9 @@ def get_precision_config():
     # DEBUG
     print('Reading precision configuration file...')
 
-    with open('config/precision.txt', 'r') as precision_file:
+    with open(f'{pathlib.Path(__file__).parent.resolve()}/config/precision.txt', 'r') as precision_file:
         precision_config = int(precision_file.read())
         return precision_config
 
-
 MP_PRECISION = get_precision_config()
+print(f'mp.dps is set to {MP_PRECISION}')
--- pynever/tensors.py
+++ pynever/tensors.py
@@ -23,14 +23,19 @@ def zeros(shape: tuple[int]) -> mpmath.matrix:
     return mpmath.zeros(*shape)
 
 
-def reshape(in_tensor: Tensor, new_shape: int | tuple[int]) -> mpmath.matrix:
-    raise NotImplementedError
+def reshape_2d(in_tensor: mpmath.matrix, new_shape: tuple[int]) -> mpmath.matrix:
+    """
+    Procedure to reshape a 2d matrix
+    (found on https://groups.google.com/g/mpmath/c/hvVeyEtZOMg)
 
-    # def reshape(mat, m, n):  reshape 2d
-    #     ...
-    #     z = sum(mat.tolist(), [])
-    #     ...
-    #     return matrix([z[n * k:n * k + n] for k in range(m)])
+    """
+
+    z = sum(in_tensor.tolist(), [])
+
+    rows = new_shape[0]
+    cols = new_shape[1]
+
+    return mpmath.matrix([z[cols * k:cols * k + cols] for k in range(rows)])
 
 
 def array(array_like: list | mpmath.matrix) -> mpmath.matrix:
@@ -45,9 +50,53 @@ def matmul(x1: mpmath.matrix, x2: mpmath.matrix) -> mpmath.matrix:
     return x1 * x2
 
 
-def vstack(tup: tuple[mpmath.matrix]) -> mpmath.matrix:
-    raise NotImplementedError
+def vstack_2d(tup: tuple[mpmath.matrix]) -> mpmath.matrix:
+    """
+    Procedure to stack two tensors vertically
+
+    """
+
+    # Check compatibility
+    cols = tup[0].cols
+    for t in tup:
+        if t.cols != cols:
+            raise Exception('Incorrect dimensions to stack')
+
+    # Create copy of first element
+    new_tensor = tup[0].copy()
+
+    # Add dimensions and data
+    for t in tup[1:]:
+        j = new_tensor.rows
+        new_tensor.rows = j + t.rows
+
+        for i in range(t.rows):
+            new_tensor[j + i, :] = t[i, :]
+
+    return new_tensor
 
 
-def hstack(tup: tuple[mpmath.matrix]) -> mpmath.matrix:
-    raise NotImplementedError
+def hstack_2d(tup: tuple[mpmath.matrix]) -> mpmath.matrix:
+    """
+    Procedure to stack two tensors horizontally
+
+    """
+
+    # Check compatibility
+    rows = tup[0].rows
+    for t in tup:
+        if t.rows != rows:
+            raise Exception('Incorrect dimensions to stack')
+
+    # Create copy of first element
+    new_tensor = tup[0].copy()
+
+    # Add dimensions and data
+    for t in tup[1:]:
+        j = new_tensor.cols
+        new_tensor.cols = j + t.cols
+
+        for i in range(t.cols):
+            new_tensor[:, j + i] = t[:, i]
+
+    return new_tensor
